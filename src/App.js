f137d2f960968eb652fea607d469aa5c7190373d
Julio.S <just.julio95@gmail.com>
2023-08-19 16:41:02 -0400

    set up the beginnings of the NAV bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import Directory from './directory/directory.component';
-import categories from './components/category-menu/category-menu.component';
+import { Routes, Route } from 'react-router-dom'
+
+import Navigation from './routes/navigation/navigation.component';
+import Home from "./routes/home/home.components";
 
 const App = () => {
+  const Shop = () => {
+    return (<h1>I AM THE SHOP PAGE</h1>)
+  }
+
   return (
-    <Directory categories={categories}/>
-  );
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
+    </Routes>
+  )
 }
 
 export default App;
