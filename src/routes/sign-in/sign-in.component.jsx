e04ea9e75b64b23b89ea212ca6dfdc39a5a77134
Julio.S <just.julio95@gmail.com>
2023-08-22 13:55:59 -0400

    created a sign up form using email and password

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,4 +1,13 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+
+import { 
+  auth,
+  signInWithGooglePopup,
+  createUserDocumentFromAuth 
+} from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
   const logGoogleUser = async () => {
@@ -9,9 +18,8 @@ const SignIn = () => {
   return (
     <div>
       <h1>SIGN IN PAGE</h1>
-      <button onClick={logGoogleUser}>
-        Sign in with Google Popup
-      </button>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <SignUpForm/>
     </div>
   )
 }
